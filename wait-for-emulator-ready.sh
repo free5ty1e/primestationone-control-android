08899730967595ccf4eaa65e90bd596e33368659
Chris Paiano <chris.paiano@gmail.com>
2016-11-24 17:11:15 -0800

    +More verbose waiting for emulator script

--- wait-for-emulator-ready.sh
+++ wait-for-emulator-ready.sh
@@ -3,7 +3,7 @@ local bootanim=""
 local failcounter=0
 until [[ "$bootanim" =~ "stopped" ]]; do
    bootanim=`adb hell getprop init.svc.bootanim 2>&1`
-   echo "Waiting: $bootanim"
+   echo "Waiting because the boot animation status is: $bootanim"
    if [[ "$bootanim" =~ "not found" ]]; then
       let "failcounter += 1"
       if [[ ${failcounter} -gt 1200 ]]; then
