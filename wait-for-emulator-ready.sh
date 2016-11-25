ea1d51f6f1fe7a3b5c04e68ab8dd80d9f746e5f4
Chris Paiano <chris.paiano@gmail.com>
2016-11-24 16:58:10 -0800

    Wait for emu script try 2

--- wait-for-emulator-ready.sh
+++ wait-for-emulator-ready.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
-wait-for-device-ready
-
-function wait-for-device-ready {
-    local bootanim=""
-    local failcounter=0
-    until [[ "$bootanim" =~ "stopped" ]]; do
-       bootanim=`adb hell getprop init.svc.bootanim 2>&1`
-       echo "$bootanim"
-       if [[ "$bootanim" =~ "not found" ]]; then
-          let "failcounter += 1"
-          if [[ ${failcounter} -gt 1200 ]]; then
-            echo "Failed to start emulator"
-            exit 1
-          fi
-       fi
-       sleep 1
-    done
-    sleep 30
-    adb shell input keyevent 82 &
-    sleep 150
-    echo "Done"
-}
+local bootanim=""
+local failcounter=0
+until [[ "$bootanim" =~ "stopped" ]]; do
+   bootanim=`adb hell getprop init.svc.bootanim 2>&1`
+   echo "Waiting: $bootanim"
+   if [[ "$bootanim" =~ "not found" ]]; then
+      let "failcounter += 1"
+      if [[ ${failcounter} -gt 1200 ]]; then
+        echo "Failed to start emulator"
+        exit 1
+      fi
+   fi
+   sleep 1
+done
+sleep 30
+adb shell input keyevent 82 &
+sleep 150
+echo "Done"
